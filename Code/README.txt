8911e8c0b01137836058c245e82d3eca13f86b4c
Abrar Abir <abrarabir18@gmail.com>
2022-11-15 20:14:45 -0800

    Delete Code directory

--- Code/README.txt
+++
@@ -1,5 +0,0 @@
-Python version: 3.9.12
-
-Steps to run the code:
-1. Place data files into the data folder.
-2. run 'run_code.sh' script.
--- Code/run_code.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-echo 'Running code for 5.1 Binary Classification'
-python q51.py
-echo 'Running code for 5.2 Multi-Class Classification'
-python q52.py
-
-echo 'finish!'
